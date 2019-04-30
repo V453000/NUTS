42896e57b71ee1a56ad8fdda60ca5311379a42a0
Vaclav Benc <vaclav.benc@factorio.com>
2019-04-30 21:33:02 +0200

    Caboose, fixes, powered switch for short.

--- scripts/generate-freight-graphics.py
+++ scripts/generate-freight-graphics.py
@@ -23,11 +23,11 @@ with open('generated-freight-short-graphics.nml', 'w') as output_nml:
       spritesheet = 'switch_rail_freight_short_vehid_back;\n'
       default_spritesheet = '  switch_rail_freight_short_vehid_wagons_self;\n'
       
-      for n in range(0, int(i_test*2)):
-        if (n%2)==0:
-          spritesheet = 'sprite_INVISIBLE;\n'
-        else:
-          spritesheet = 'switch_rail_freight_short_vehid_back;\n'
+      for n in range(0, int(i_test*1)):
+        #if (n%2)==0:
+        #  spritesheet = 'sprite_INVISIBLE;\n'
+        #else:
+        spritesheet = 'switch_rail_freight_short_vehid_back;\n'
         output_nml.write('  ' + str(n) + ': ' + spritesheet)
       output_nml.write(default_spritesheet)
       output_nml.write('}\n')
