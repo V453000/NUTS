3ecdf89ec091ca373e0ebe5840ce9aa839619e36
V453000 <vaclav.benc@gmail.com>
2021-06-08 22:19:01 +0200

    Reverted adding the 15 PURR types.

--- scripts/run-index_replacer.bat
+++ scripts/run-index_replacer.bat
@@ -1,58 +1,65 @@
 @REM Primary recolours
-index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_1.png
-index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_2.png
-index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_3.png
-index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_4.png
-index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_5.png
+python index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_1.png
+python index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_2.png
+python index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_3.png
+python index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_4.png
+python index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_5.png
 
-index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_1.png
-index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_2.png
-index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_3.png
-index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_4.png
-index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_5.png
+@REM @REM Primary recolours
+@REM index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_1.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_2.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_3.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_4.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_RAIL_5.png
 
-index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_1.png
-index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_2.png
-index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_3.png
-index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_4.png
-index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_5.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_1.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_2.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_3.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_4.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MONO_5.png
 
-@REM Secondary recolours
+@REM index_replacer.py -a (19,20,21,22,23) -b (19,20,21,22,23)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_1.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (63,64,65,66,67)      -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_2.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (180,181,182,183,184) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_3.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (147,148,149,150,151) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_4.png
+@REM index_replacer.py -a (19,20,21,22,23) -b (85,206,207,208,209)  -i ../gfx/_PURR_original.png -o ../gfx/_PURR_MGLV_5.png
+
+@REM @REM Secondary recolours
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_1.png -o ../gfx/_PURR_RAIL_1.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_2.png -o ../gfx/_PURR_RAIL_2.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_3.png -o ../gfx/_PURR_RAIL_3.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_4.png -o ../gfx/_PURR_RAIL_4.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_5.png -o ../gfx/_PURR_RAIL_5.png
+
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_1.png -o ../gfx/_PURR_MONO_1.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_2.png -o ../gfx/_PURR_MONO_2.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_3.png -o ../gfx/_PURR_MONO_3.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_4.png -o ../gfx/_PURR_MONO_4.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_5.png -o ../gfx/_PURR_MONO_5.png
+
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_1.png -o ../gfx/_PURR_MGLV_1.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_2.png -o ../gfx/_PURR_MGLV_2.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_3.png -o ../gfx/_PURR_MGLV_3.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_4.png -o ../gfx/_PURR_MGLV_4.png
+@REM @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_5.png -o ../gfx/_PURR_MGLV_5.png
+
 @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_1.png -o ../gfx/_PURR_RAIL_1.png
 @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_2.png -o ../gfx/_PURR_RAIL_2.png
 @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_3.png -o ../gfx/_PURR_RAIL_3.png
 @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_4.png -o ../gfx/_PURR_RAIL_4.png
 @REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_5.png -o ../gfx/_PURR_RAIL_5.png
 
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_1.png -o ../gfx/_PURR_MONO_1.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_2.png -o ../gfx/_PURR_MONO_2.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_3.png -o ../gfx/_PURR_MONO_3.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_4.png -o ../gfx/_PURR_MONO_4.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (3,4,5,6,7,8,9,10) -i ../gfx/_PURR_MONO_5.png -o ../gfx/_PURR_MONO_5.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_1.png -o ../gfx/_PURR_MONO_1.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_2.png -o ../gfx/_PURR_MONO_2.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_3.png -o ../gfx/_PURR_MONO_3.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_4.png -o ../gfx/_PURR_MONO_4.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_5.png -o ../gfx/_PURR_MONO_5.png
 
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_1.png -o ../gfx/_PURR_MGLV_1.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_2.png -o ../gfx/_PURR_MGLV_2.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_3.png -o ../gfx/_PURR_MGLV_3.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_4.png -o ../gfx/_PURR_MGLV_4.png
-@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (5,6,7,8,9,10,11,12,13) -i ../gfx/_PURR_MGLV_5.png -o ../gfx/_PURR_MGLV_5.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_1.png -o ../gfx/_PURR_MGLV_1.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_2.png -o ../gfx/_PURR_MGLV_2.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_3.png -o ../gfx/_PURR_MGLV_3.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_4.png -o ../gfx/_PURR_MGLV_4.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_5.png -o ../gfx/_PURR_MGLV_5.png
 
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_1.png -o ../gfx/_PURR_RAIL_1.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_2.png -o ../gfx/_PURR_RAIL_2.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_3.png -o ../gfx/_PURR_RAIL_3.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_4.png -o ../gfx/_PURR_RAIL_4.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,2,3,4,5,6,7,8) -i ../gfx/_PURR_RAIL_5.png -o ../gfx/_PURR_RAIL_5.png
-
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_1.png -o ../gfx/_PURR_MONO_1.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_2.png -o ../gfx/_PURR_MONO_2.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_3.png -o ../gfx/_PURR_MONO_3.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_4.png -o ../gfx/_PURR_MONO_4.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (32,33,34,56,57,58,59,38) -i ../gfx/_PURR_MONO_5.png -o ../gfx/_PURR_MONO_5.png
-
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_1.png -o ../gfx/_PURR_MGLV_1.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_2.png -o ../gfx/_PURR_MGLV_2.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_3.png -o ../gfx/_PURR_MGLV_3.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_4.png -o ../gfx/_PURR_MGLV_4.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (128,129,130,131,132,133,134,135) -i ../gfx/_PURR_MGLV_5.png -o ../gfx/_PURR_MGLV_5.png
-
-index_replacer.py -a (19,20,21,22,23,24,25,26) -b (170,171,172,173,174,175,176,177) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_UNIV.png
-index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,1,1,1,2,3,4,5) -i ../gfx/_PURR_UNIV.png -o ../gfx/_PURR_UNIV.png
+@REM index_replacer.py -a (19,20,21,22,23,24,25,26) -b (170,171,172,173,174,175,176,177) -i ../gfx/_PURR_original.png -o ../gfx/_PURR_UNIV.png
+@REM index_replacer.py -a (1,2,3,4,5,6,7,8) -b (1,1,1,1,2,3,4,5) -i ../gfx/_PURR_UNIV.png -o ../gfx/_PURR_UNIV.png
